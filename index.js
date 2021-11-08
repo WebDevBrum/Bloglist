6e993b6b5f34de2ca88ad9550ad75b376a2151f2
Scott Davies <scottdavies1983@gmail.com>
2021-11-08 06:58:31 +0000

    further npm project setup

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const http = require("http");
 const express = require("express");
 
 const app = express();
+const morgan = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
 
@@ -14,11 +16,51 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = "mongodb://localhost/bloglist";
-mongoose.connect(mongoUrl);
+const mongoUrl = process.env.MONGODB_URI;
+mongoose
+  .connect(mongoUrl)
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 app.use(cors());
 app.use(express.json());
+
+morgan.token("data", (req, res) => {
+  const { body } = req; // destruct
+
+  return JSON.stringify(body);
+});
+
+morgan((tokens, req, res) =>
+  [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"),
+    "-",
+    tokens["response-time"](req, res),
+    "ms",
+    // tokens.data(req, res)
+  ].join(" ")
+);
+
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+);
+
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+app.use(requestLogger);
 
 app.get("/api/blogs", (request, response) => {
   Blog.find({}).then((blogs) => {
