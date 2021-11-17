92ef758f6865d7f6c1c658c6fe8c5ccdc98b8f2b
Scott Davies <scottdavies1983@gmail.com>
2021-11-17 07:06:00 +0000

    test for most blogs, 4.6*: helper functions and unit tests, step4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,7 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-shadow */
+const blog = require("../models/blog");
+
 const dummy = (blogs) => 1;
 
 const totalLikes = (blogs) => {
@@ -29,8 +33,38 @@ const favoriteBlog = (blogs) => {
   return favorite;
 };
 
+const mostBlogs = (bloglist) => {
+  let blogCount = {};
+  let blogMax = 0;
+  let authorDetails = { author: null, blogs: null };
+
+  bloglist.forEach((blog) => {
+    blogCount[blog.author] = blog.author;
+
+    if (blogCount[`${blog.author}count`]) {
+      blogCount[`${blog.author}count`] += 1;
+    } else {
+      blogCount[`${blog.author}count`] = 1;
+    }
+  });
+
+  const authors = Object.keys(blogCount);
+
+  authors.forEach((entry) => {
+    if (blogCount[`${entry}count`] > blogMax) {
+      blogMax = blogCount[`${entry}count`];
+      authorDetails.author = entry;
+      authorDetails.blogs = blogCount[`${entry}count`];
+    }
+  });
+
+  return authorDetails;
+  // Now how would lodash help here??
+};
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 };
