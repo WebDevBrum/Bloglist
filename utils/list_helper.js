aca47c83db737aabab4f70bd524e420a238faa56
Scott Davies <scottdavies1983@gmail.com>
2021-11-17 05:50:13 +0000

    tests for favorite blogs

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,27 @@ const totalLikes = (blogs) => {
   return count;
 };
 
+const favoriteBlog = (blogs) => {
+  let mostLikes = 0;
+  let favorite = {};
+
+  // { title: "none", author: "none", likes: 12 };
+
+  blogs.forEach((blog) => {
+    if (blog.likes > mostLikes) {
+      mostLikes = blog.likes;
+      favorite = blog;
+      // favorite.title = blog.title;
+      // favorite.author = blog.author;
+      // favorite.likes = blog.likes;
+    }
+  });
+
+  return favorite;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
